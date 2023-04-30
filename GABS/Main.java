9007438d5d6c5e7e17e6f4f9d6af11b38905ebe4
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2023-04-30 22:38:49 +0800

    Update Main.java - Lorenz

--- GABS/Main.java
+++ GABS/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String [] args) {
 		
-		IntroGUI introGUI = new IntroGUI();
+//		IntroGUI introGUI = new IntroGUI();
+		AdminLoginGUI adminLogin = new AdminLoginGUI();
 		
 	}
 	
