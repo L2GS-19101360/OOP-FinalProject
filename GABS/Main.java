f65497c2cb82b33a9cdb60063a74b2317f46115f
Lorenz Gil Suico <125742061+L2GS-19101360@users.noreply.github.com>
2023-05-02 08:02:56 +0800

    Add files via upload

---
+++ GABS/Main.java
@@ -0,0 +1,12 @@
+package GABS;
+
+public class Main {
+
+	public static void main(String [] args) {
+		
+		IntroGUI introGUI = new IntroGUI();
+//		AdminLoginGUI adminLogin = new AdminLoginGUI();
+		
+	}
+	
+}
